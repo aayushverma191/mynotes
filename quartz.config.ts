a62a5c7cc21e989a906c6d339fbc2dc519ac4dcc
aayushverma1910 <aayush.verma@mygurukulam.co>
2025-06-24 23:59:07 +0530

    Quartz sync: Jun 24, 2025, 11:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 DEEPAK NISHAD",
+    pageTitle: "🪴 Aayush Verma",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
